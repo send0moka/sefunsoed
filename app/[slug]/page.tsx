5db8ffd0afb2486195f3a112c1e6b18def007d22
send0moka <sendomoka2@gmail.com>
2025-03-27 23:25:44 +0700

    feat(page): update Page component to handle optional searchParams and improve metadata generation

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,18 +1,18 @@
-import { Metadata } from 'next';
-import ClientPage from './ClientPage';
+import { Metadata } from "next"
+import ClientPage from "./ClientPage"
 
 type Props = {
-  params: { slug: string },
-  searchParams: { [key: string]: string | string[] | undefined }
+  params: { slug: string }
+  searchParams?: { [key: string]: string | string[] | undefined }
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title: `Page: ${params.slug}`,
     // Add other metadata as needed
-  };
+  }
 }
 
-export default function Page({ params }: Props) {
-  return <ClientPage slug={params.slug} />;
+export default async function Page({ params }: Props) {
+  return <ClientPage slug={params.slug} />
 }
