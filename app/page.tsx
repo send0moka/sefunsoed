e37759ecf8b3afd5e763564c50ab1e47398cca02
send0moka <sendomoka2@gmail.com>
2025-04-25 12:26:39 +0700

    feat: update home hero section layout and add new SVG assets

--- app/page.tsx
+++ app/page.tsx
@@ -20,146 +20,134 @@ function HomeComponent() {
   return (
     <div className="bg-white">
       {/* Hero section */}
-      <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20">
-        <div className="mx-auto mt-10 sm:mt-24 lg:mt-32 max-w-7xl px-6 py-12 sm:py-16 lg:py-24 lg:px-8">
-          {/* Hero Header */}
-          <div className="flex flex-col lg:flex-row lg:items-end sm:gap-x-6">
-            <h1 className="text-[2.12rem] sm:text-5xl lg:text-7xl font-semibold lg:font-bold tracking-tighter lg:tracking-tight text-gray-900">
-              {translations[language].home.hero.title}
-            </h1>
-            <div className="flex flex-col lg:flex-row -space-x-6 mt-1 lg:mt-0 gap-1 lg:gap-0">
-              <div className="flex gap-3 items-center">
-                <Image 
-                  src="/lamp.svg" 
-                  alt="Lamp" 
-                  width={125} 
-                  height={125}
-                  className="size-12 sm:size-24 lg:size-32" 
-                />
-                <h1 className="lg:hidden text-[2.12rem] sm:text-6xl lg:text-7xl font-semibold lg:font-bold tracking-tighter lg:tracking-wide text-indigo-500">
-                  {translations[language].home.hero.subtitle}
-                </h1>
-              </div>
-              <div className="flex gap-3 items-center">
-                <Image 
-                  src="/star.svg" 
-                  alt="star" 
-                  width={125} 
-                  height={125}
-                  className="size-12 sm:size-24 lg:size-32" 
-                />
-                <h1 className="lg:hidden -translate-y-0.5 text-[2.12rem] sm:text-5xl lg:text-7xl font-semibold lg:font-bold tracking-tighter lg:tracking-tight text-gray-900">
-                  {translations[language].home.hero.place}
-                </h1>
-                <Image 
-                  src="/lup.svg" 
-                  alt="lup" 
-                  width={80} 
-                  height={80}
-                  className="lg:hidden size-10 -translate-y-0.5" 
-                />
-              </div>
+      <div className="mx-auto mt-10 sm:mt-24 lg:mt-32 max-w-7xl px-6 py-12 sm:py-16 lg:py-24 lg:px-8">
+        {/* Hero Header */}
+        <div className="flex flex-col lg:flex-row lg:items-end sm:gap-x-6">
+          <h1 className="text-[2.12rem] sm:text-5xl lg:text-7xl font-semibold lg:font-bold tracking-tighter lg:tracking-tight text-gray-900">
+            {translations[language].home.hero.title}
+          </h1>
+          <div className="flex flex-col lg:flex-row -space-x-6 mt-1 lg:mt-0 gap-1 lg:gap-0">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="/lamp.svg"
+                alt="Lamp"
+                width={125}
+                height={125}
+                className="size-12 sm:size-24 lg:size-32"
+              />
+              <h1 className="lg:hidden text-[2.12rem] sm:text-6xl lg:text-7xl font-semibold lg:font-bold tracking-tighter lg:tracking-wide text-indigo-500">
+                {translations[language].home.hero.subtitle}
+              </h1>
+            </div>
+            <div className="flex gap-3 items-center">
+              <Image
+                src="/star.svg"
+                alt="star"
+                width={125}
+                height={125}
+                className="size-12 sm:size-24 lg:size-32"
+              />
+              <h1 className="lg:hidden -translate-y-0.5 text-[2.12rem] sm:text-5xl lg:text-7xl font-semibold lg:font-bold tracking-tighter lg:tracking-tight text-gray-900">
+                {translations[language].home.hero.place}
+              </h1>
+              <Image
+                src="/lup.svg"
+                alt="lup"
+                width={80}
+                height={80}
+                className="lg:hidden size-10 -translate-y-0.5"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Hero Content */}
+        <div className="flex flex-col-reverse lg:flex-row mt-2 lg:mt-8 gap-8 lg:gap-x-8">
+          {/* Left Column */}
+          <div className="flex flex-col gap-y-6 items-center lg:items-end">
+            <div className="w-full lg:w-fit flex flex-row -space-y-4 sm:-space-x-6 sm:space-y-0 items-center sm:items-end">
+              <Image
+                src="/laptop.png"
+                alt="laptop"
+                width={600}
+                height={300}
+                className="absolute -left-2 lg:left-0 bottom-40 lg:bottom-0 lg:relative w-24 sm:w-28 lg:w-32 z-10"
+              />
+              <Image
+                src="/forum.png"
+                alt="Forum"
+                width={600}
+                height={300}
+                className="w-full lg:w-auto h-48 lg:h-72 rounded-3xl object-cover"
+              />
+            </div>
+            <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-x-6">
+              <Link
+                href={getLinkWithLang("/registration")}
+                className="w-full sm:w-fit rounded-full bg-indigo-600 px-6 py-3 text-base lg:text-lg font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {translations[language].home.hero.registerButton}
+              </Link>
+              <Link
+                href={getLinkWithLang("/about")}
+                className="text-base lg:text-lg font-semibold leading-6 text-gray-900"
+              >
+                {translations[language].home.hero.learnMore}{" "}
+                <span aria-hidden="true">→</span>
+              </Link>
             </div>
           </div>
 
-          {/* Hero Content */}
-          <div className="flex flex-col-reverse lg:flex-row mt-2 lg:mt-8 gap-8 lg:gap-x-8">
-            {/* Left Column */}
-            <div className="flex flex-col gap-y-6 items-center lg:items-end">
-              <div className="w-full lg:w-fit flex flex-row -space-y-4 sm:-space-x-6 sm:space-y-0 items-center sm:items-end">
-                <Image
-                  src="/laptop.png"
-                  alt="laptop"
-                  width={600}
-                  height={300}
-                  className="absolute -left-2 lg:left-0 bottom-40 lg:bottom-0 lg:relative w-24 sm:w-28 lg:w-32 z-10"
-                />
-                <Image
-                  src="/forum.png"
-                  alt="Forum"
-                  width={600}
-                  height={300}
-                  className="w-full lg:w-auto h-48 lg:h-72 rounded-3xl object-cover"
-                />
-              </div>
-              <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-x-6">
-                <Link
-                  href={getLinkWithLang("/registration")}
-                  className="w-full sm:w-fit rounded-full bg-indigo-600 px-6 py-3 text-base lg:text-lg font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  {translations[language].home.hero.registerButton}
-                </Link>
-                <Link
-                  href={getLinkWithLang("/about")}
-                  className="text-base lg:text-lg font-semibold leading-6 text-gray-900"
-                >
-                  {translations[language].home.hero.learnMore}{" "}
-                  <span aria-hidden="true">→</span>
-                </Link>
-              </div>
-            </div>
-
-            {/* Right Column */}
-            <div className="space-y-4">
-              <h1 className="hidden lg:flex text-5xl sm:text-6xl lg:text-7xl font-bold tracking-wide text-indigo-500">
-                {translations[language].home.hero.institution}
+          {/* Right Column */}
+          <div className="space-y-4">
+            <h1 className="hidden lg:flex text-5xl sm:text-6xl lg:text-7xl font-bold tracking-wide text-indigo-500">
+              {translations[language].home.hero.institution}
+            </h1>
+            <div className="hidden lg:flex flex-col sm:flex-row items-center sm:items-start gap-4 sm:gap-x-4">
+              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold tracking-tight text-gray-900">
+                {translations[language].home.hero.place}
               </h1>
-              <div className="hidden lg:flex flex-col sm:flex-row items-center sm:items-start gap-4 sm:gap-x-4">
-                <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold tracking-tight text-gray-900">
-                  {translations[language].home.hero.place}
-                </h1>
-                <Image 
-                  src="/lup.svg" 
-                  alt="lup" 
-                  width={80} 
-                  height={80}
-                  className="w-16 sm:w-20 lg:w-24 h-auto" 
-                />
-              </div>
-              <p className="text-base lg:text-lg font-medium text-balance max-w-2xl mx-auto lg:mx-0 lg:leading-8 text-gray-500 lg:text-gray-900">
-                {translations[language].home.hero.description}
-              </p>
+              <Image
+                src="/lup.svg"
+                alt="lup"
+                width={80}
+                height={80}
+                className="w-16 sm:w-20 lg:w-24 h-auto"
+              />
             </div>
+            <p className="text-base lg:text-lg font-medium text-balance max-w-2xl mx-auto lg:mx-0 lg:leading-8 text-gray-500 lg:text-gray-900">
+              {translations[language].home.hero.description}
+            </p>
           </div>
         </div>
       </div>
 
       {/* Feature section */}
-      <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">
-            {translations[language].home.features.title}
-          </h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            {translations[language].home.features.subtitle}
-          </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            {translations[language].home.features.description}
-          </p>
+      <div className="bg-gray-100">
+        <div className="pt-10 mx-auto mt-32 max-w-7xl">
+          <div className="flex flex-col items-center">
+            <div className="relative">
+              <h1 className="font-bold text-3xl lg:text-4xl text-center">
+                {translations[language].home.features.title}
+              </h1>
+              <Image 
+                src="/crown.svg" 
+                alt="crown" 
+                width={100} 
+                height={100} 
+                className="hidden lg:block absolute -top-9 -right-9 size-14" 
+              />
+              <Image 
+                src="/z-line.svg" 
+                alt="zigzag" 
+                width={1000} 
+                height={100}
+                className="lg:hidden absolute -bottom-7 left-0 w-full h-auto" 
+              />
+            </div>
+          </div>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-            {features.map((feature) => (
-              <div key={feature.name} className="flex flex-col">
-                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
-                  <feature.icon
-                    className="h-5 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  />
-                  {translations[language].home.features[feature.key].title}
-                </dt>
-                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
-                  <p className="flex-auto">
-                    {
-                      translations[language].home.features[feature.key]
-                        .description
-                    }
-                  </p>
-                </dd>
-              </div>
-            ))}
-          </dl>
-        </div>
+        <div className="w-full h-96 bg-black/90 mt-10" style={{ clipPath: 'polygon(0 10%, 100% 0, 100% 100%, 0 100%)' }}></div>
       </div>
 
       {/* CTA section */}
