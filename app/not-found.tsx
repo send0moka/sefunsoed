48969b8ca6ae6219ad929f2df3e760a972f1728a
send0moka <sendomoka2@gmail.com>
2025-06-15 08:49:38 +0700

    feat(not-found): enhance NotFound component layout and styling with image and improved structure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from "next/link"
+import Image from "next/image"
 import { useLanguage } from "@/contexts/LanguageContext"
 import { translations } from "@/translations"
 import { useSearchParams } from "next/navigation"
@@ -17,21 +18,27 @@ function NotFoundContent() {
   }
 
   return (
-    <div className="bg-white px-6 py-32 sm:py-40 lg:px-8">
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+    <div className="grid min-h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <Image
+          src="/images/404.svg"
+          alt="404"
+          width={400}
+          height={400}
+          className="mx-auto"
+        />
+        <h2 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           {translations[language].content.notFound.title}
         </h2>
-        <p className="mt-6 text-lg leading-8 text-gray-600">
+        <p className="mt-6 text-base leading-7 text-gray-600">
           {translations[language].content.notFound.description}
         </p>
-        <div className="mt-10">
+        <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             href={getLinkWithLang("/")}
-            className="text-sm font-semibold leading-6 text-indigo-600"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            {translations[language].content.notFound.backToHome}{" "}
-            <span aria-hidden="true">→</span>
+            {translations[language].content.notFound.backToHome}
           </Link>
         </div>
       </div>
