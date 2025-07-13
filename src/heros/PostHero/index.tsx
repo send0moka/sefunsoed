9577f0d4a9fc2983e202f07e521953d2136879d1
send0moka <sendomoka2@gmail.com>
2025-07-14 05:48:52 +0700

    style: improve layout and formatting in PostHero component

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -17,8 +17,8 @@ export const PostHero: React.FC<{
   return (
     <div className="relative -mt-[10.4rem] flex items-end">
       <div className="container z-10 relative lg:grid lg:grid-cols-[1fr_48rem_1fr] text-white pb-8">
-        <div className="col-start-1 col-span-1 md:col-start-2 md:col-span-2">
-          <div className="uppercase text-sm mb-6">
+        <div className="col-span-1 col-start-1 md:col-start-2 md:col-span-2">
+          <div className="mb-6 text-sm uppercase">
             {categories?.map((category, index) => {
               if (typeof category === 'object' && category !== null) {
                 const { title: categoryTitle } = category
@@ -39,10 +39,10 @@ export const PostHero: React.FC<{
           </div>
 
           <div className="">
-            <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl">{title}</h1>
+            <h1 className="mb-6 text-3xl font-semibold md:text-5xl lg:text-6xl">{title}</h1>
           </div>
 
-          <div className="flex flex-col md:flex-row gap-4 md:gap-16">
+          <div className="flex flex-col gap-4 md:flex-row md:gap-16">
             {hasAuthors && (
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col gap-1">
@@ -67,9 +67,9 @@ export const PostHero: React.FC<{
           <Media fill priority imgClassName="-z-10 object-cover" resource={heroImage} />
         )}
         {/* Gradient overlay at the top */}
-        <div className="absolute pointer-events-none left-0 top-0 w-full h-20 bg-gradient-to-b from-black to-transparent" />
+        <div className="absolute top-0 left-0 w-full h-20 pointer-events-none bg-gradient-to-b from-black to-transparent" />
         {/* Gradient overlay at the bottom */}
-        <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
+        <div className="absolute bottom-0 left-0 w-full pointer-events-none h-1/2 bg-gradient-to-t from-black to-transparent" />
       </div>
     </div>
   )
